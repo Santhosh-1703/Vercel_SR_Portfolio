c4a8669ffd5a882c55d9768f11a69936c3b03b05
Santhosh-1703 <rajendransanthosh8@gmail.com>
2024-08-08 17:26:18 +0530

    vc15

--- portfolioSR/settings.py
+++ portfolioSR/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8yu5ymt2yfwxvi0f^(srs!_ifmr687x0zwp+lv2xvaa-_xv!!-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','.vercel.app','now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -67,7 +67,6 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- portfolioSR/urls.py
+++ portfolioSR/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('portfolioappsr.urls')),
     path('accounts/',include('allauth.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 
